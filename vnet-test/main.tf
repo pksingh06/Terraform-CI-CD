b5774b610d02a861d14d9128c0fbef70b2f8bfdc
Pramod Kumar <42300697+pksingh06@users.noreply.github.com>
2020-05-28 12:41:01 +0530

    Update main.tf

--- vnet-test/main.tf
+++ vnet-test/main.tf
@@ -2,18 +2,28 @@ provider "azurerm" {
   version = "2.2.0"
   features {}
 }
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg-name
+resource "azurerm_resource_group" "r" {
+  name     = "${var.rg-location}-${var.rg-name}"
   location = var.rg-location
 }
 
 resource "azurerm_virtual_network" "vn" {
   count               = length(var.vnet-prefix)
-  name                = "${azurerm_resource_group.rg.name}-${var.vnet-prefix[count.index]}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  name                = "${azurerm_resource_group.r.name}-${var.vnet-prefix[count.index]}"
+  resource_group_name = azurerm_resource_group.r.name
+  location            = azurerm_resource_group.r.location
   address_space       = [var.vnet-address[count.index]]
 }
+
+resource "azurerm_virtual_network_peering" "vnp" {
+  count                     = length(var.vnet-peer)
+  name                      = var.vnet-peer[count.index]
+  resource_group_name       = azurerm_resource_group.r.name
+  virtual_network_name      = count.index == 0 ? azurerm_virtual_network.vn[0].name : azurerm_virtual_network.vn[1].name
+  remote_virtual_network_id = count.index == 0 ? azurerm_virtual_network.vn[1].id : azurerm_virtual_network.vn[0].id
+
+}
+
 
 
 
